ffd04ee799b16eb7c97b88a872af1c4030db55ee
Hugoq <sir.hugo777@fpuna.edu.py>
2024-08-28 15:55:35 -0400

    staticdir-config

--- SERVER/settings.py
+++ SERVER/settings.py
@@ -187,10 +187,10 @@ DATE_INPUT_FORMATS = ['%d/%m/%Y']
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-]
+] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
