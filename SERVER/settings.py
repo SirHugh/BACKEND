9db3d7d13235e882d710585c25d50994c4402f2a
Hugoq <sir.hugo777@fpuna.edu.py>
2024-08-29 10:32:29 -0400

    static_root-changed

--- SERVER/settings.py
+++ SERVER/settings.py
@@ -187,7 +187,7 @@ DATE_INPUT_FORMATS = ['%d/%m/%Y']
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
