b3af056360e436d20ae31a28e6d4e2f55fb4365b
Hugoq <sir.hugo777@fpuna.edu.py>
2024-08-31 13:58:38 -0400

    corsalloworigin-change

--- SERVER/settings.py
+++ SERVER/settings.py
@@ -212,7 +212,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://localhost:5174",
+    "http://localhost:5174", "https://sige-upc.netlify.app",
 ]
 
 # Actual directory user files go to
